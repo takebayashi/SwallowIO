b3b5f456b8691674aabb401a534fbfdcbd309618
Shun Takebayashi <shun@takebayashi.asia>
2016-06-12 16:10:26 +0900

    Update C7 to 0.9

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwallowIO",
     dependencies: [
-      .Package(url: "https://github.com/open-swift/C7.git", versions: Version(0, 8, 0)..<Version(1, 0, 0)),
+      .Package(url: "https://github.com/open-swift/C7.git", versions: Version(0, 9, 0)..<Version(0, 10, 0)),
     ]
 )
