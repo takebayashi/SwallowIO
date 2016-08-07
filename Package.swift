781fc878a9c7fe0a2dea9b047eb40aa7d060a3a7
Shun Takebayashi <shun@takebayashi.asia>
2016-08-07 16:55:18 +0900

    Update CI configs to use Swift DEVELOPMENT-SNAPSHOT-2016-07-25-a

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwallowIO",
     dependencies: [
-      .Package(url: "https://github.com/open-swift/C7.git", versions: Version(0, 9, 0)..<Version(0, 10, 0)),
+      .Package(url: "https://github.com/open-swift/C7.git", versions: Version(0, 11, 0)...Version(0, 11, 999)),
     ]
 )
