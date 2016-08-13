2ba682564628c6b9541fd1abb9b0c0882c5a8508
Shun Takebayashi <shun@takebayashi.asia>
2016-08-14 08:05:19 +0900

    Replace send syscall with write

--- Sources/SocketStream.swift
+++ Sources/SocketStream.swift
@@ -2,10 +2,8 @@ import C7
 
 #if os(OSX) || os(tvOS) || os(watchOS) || os(iOS)
     import Darwin
-    let syscall_send = Darwin.send
 #elseif os(Linux)
     import Glibc
-    let syscall_send = Glibc.send
 #endif
 
 extension FileDescriptor {
@@ -17,7 +15,7 @@ extension FileDescriptor {
                      completion: ((Void) throws -> Void) -> Void) {
         completion {
             try data.withUnsafeBufferPointer { bytes in
-                let result = syscall_send(self.rawDescriptor, bytes.baseAddress, bytes.count, 0)
+                let result = write(self.rawDescriptor, bytes.baseAddress, bytes.count)
                 if result < 0 {
                     throw IOError.GenericError(code: errno)
                 }
@@ -33,7 +31,7 @@ extension FileDescriptor {
     
     public func send(_ data: Data, timingOut deadline: Double) throws {
         try data.withUnsafeBufferPointer { bytes in
-            let result = syscall_send(self.rawDescriptor, bytes.baseAddress, bytes.count, 0)
+            let result = write(self.rawDescriptor, bytes.baseAddress, bytes.count)
             if result < 0 {
                 throw IOError.GenericError(code: errno)
             }
