3e9a5758559db7e8996fb786b8329e8e92d6c2b5
Shun Takebayashi <shun@takebayashi.asia>
2016-08-14 08:41:03 +0900

    Add file-writing test

--- Tests/SwallowIOTests/SwallowIOTests.swift
+++ Tests/SwallowIOTests/SwallowIOTests.swift
@@ -1,6 +1,11 @@
 import XCTest
 import C7
 @testable import SwallowIO
+#if os(OSX) || os(tvOS) || os(watchOS) || os(iOS)
+    import Darwin
+#elseif os(Linux)
+    import Glibc
+#endif
 
 class SwallowIOTests: XCTestCase {
     func testFileRead() {
@@ -23,6 +28,46 @@ class SwallowIOTests: XCTestCase {
         }
     }
 
+    func testFileWrite() {
+        let filename = "SwiftOutput.txt"
+        
+        // write to file
+        guard let outFile = PosixFile(name: filename, flags: [.writeOnly, .create, .truncate]) else {
+            XCTFail("failed to open " + filename)
+            return
+        }
+        do {
+            try outFile.send(C7.Data("Hello, World!"))
+            try outFile.close()
+        }
+        catch IOError.GenericError(let code) {
+            XCTFail("operation failed: " + String(cString: strerror(code)))
+        }
+        catch {
+            XCTFail("operation failed: unknown error")
+        }
+        
+        // read from file
+        guard let inFile = PosixFile(name: filename, flags: [.readOnly]) else {
+            XCTFail("failed to open " + filename)
+            return
+        }
+        do {
+            let data = try inFile.receive(upTo: 64)
+            let expected = C7.Data("Hello, World!")
+            XCTAssertEqual(data, expected)
+            try inFile.close()
+        }
+        catch IOError.GenericError(let code) {
+            XCTFail("operation failed: " + String(cString: strerror(code)))
+        }
+        catch {
+            XCTFail("operation failed: unknown error")
+        }
+        
+        // cleanup
+        unlink(filename)
+    }
 
     static var allTests : [(String, (SwallowIOTests) -> () throws -> Void)] {
         return [
