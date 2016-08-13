b83f773fc6ca21157c3bc7c87b454c9995822b3f
Shun Takebayashi <shun@takebayashi.asia>
2016-08-14 08:09:46 +0900

    Add file permissions

--- Sources/File.swift
+++ Sources/File.swift
@@ -47,14 +47,39 @@ public struct FileOperationFlag: OptionSet {
     static let truncate = FileOperationFlag(rawValue: O_TRUNC)
 }
 
+public struct FilePermissionFlag: OptionSet {
+    typealias RawRepresentable = mode_t
+    
+    public let rawValue: mode_t
+    
+    public init(rawValue: mode_t) {
+        self.rawValue = rawValue
+    }
+    
+    static let rwxUser = FilePermissionFlag(rawValue: S_IRWXU)
+    static let rUser = FilePermissionFlag(rawValue: S_IRUSR)
+    static let wUser = FilePermissionFlag(rawValue: S_IWUSR)
+    static let xUser = FilePermissionFlag(rawValue: S_IXUSR)
+    static let rwxGroup = FilePermissionFlag(rawValue: S_IRWXG)
+    static let rGroup = FilePermissionFlag(rawValue: S_IRGRP)
+    static let wGroup = FilePermissionFlag(rawValue: S_IWGRP)
+    static let xGroup = FilePermissionFlag(rawValue: S_IXGRP)
+    static let rwxOther = FilePermissionFlag(rawValue: S_IRWXO)
+    static let rOther = FilePermissionFlag(rawValue: S_IROTH)
+    static let wOther = FilePermissionFlag(rawValue: S_IWOTH)
+    static let xOther = FilePermissionFlag(rawValue: S_IXOTH)
+    
+    static let of644: FilePermissionFlag = [.rUser, .wUser, .rGroup, .rOther]
+}
+
 public class PosixFile: File {
     
     public var rawDescriptor: Int32 = 0
     public var closed: Bool
     
-    init?(name: String, flags: FileOperationFlag) {
+    init?(name: String, flags: FileOperationFlag, mode: FilePermissionFlag = .of644) {
         self.rawDescriptor = name.withCString { (string) -> Int32 in
-            return open(string, flags.rawValue)
+            return open(string, flags.rawValue, mode.rawValue)
         }
         if self.rawDescriptor < 0 {
             return nil
